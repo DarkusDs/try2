d52f2e13686676963832e7bd024ba9e2e2c48385
DarkusDS <rex.st.04@gmail.com>
2020-02-04 17:22:33 +0200

    no action bar

--- app/src/main/java/com/example/try1/MainActivity.java
+++ app/src/main/java/com/example/try1/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button_col = findViewById(R.id.button_collection);
+
         button_col.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
                 openGameMode1Activity();
             }
         });
+
+
+
 
 
     }
