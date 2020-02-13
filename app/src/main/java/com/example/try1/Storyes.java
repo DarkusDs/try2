2021b393a4dd85ad2f5c001fd64b7186d6416ffc
DarkusDS <rex.st.04@gmail.com>
2020-02-13 17:46:39 +0200

    collection + scroll

---
+++ app/src/main/java/com/example/try1/Storyes.java
@@ -0,0 +1,14 @@
+package com.example.try1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Storyes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storyes);
+    }
+}
